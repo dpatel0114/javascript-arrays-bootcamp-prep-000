004c566b8297b4563fbc6f91d571e9603ad7ab47
Dhara Patel <1991dharapatel@gmail.com>
2019-02-22 02:17:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function accessElementInArray(array, index){
   return array[index]}
   
 function destructivelyRemoveElementFromBeginningOfArray(array){
-   array.shift(array)
+   array.shift()
     return array}
 
 
