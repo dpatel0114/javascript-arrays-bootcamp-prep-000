e6f40a89df13661df722ce11dc1ea9a3978f902b
Dhara Patel <1991dharapatel@gmail.com>
2019-02-22 02:30:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)}
 
-
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
 
 
 
